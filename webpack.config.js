d7129a1d0f2d3278519fd9b6f499b1488262dce7
linguochi <382150914@qq.com>
2018-06-11 16:37:43 +0800

    1.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,26 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   mode: 'production',
-  entry: './src/main.js',
+  entry: {
+    AsyncComponent: path.resolve('./src/main.js'),
+  },
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    library: 'AsyncComponent',
+    libraryTarget: 'umd',
+  },
+
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve('./src'),
+    },
+  },
+  externals: {
+    vue: 'vue',
   },
   module: {
     rules: [
@@ -20,6 +36,8 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
+        include: [path.resolve('./src')],
+        exclude: /node_modules/,
       },
       // 它会应用到普通的 `.css` 文件
       // 以及 `.vue` 文件中的 `<style>` 块
