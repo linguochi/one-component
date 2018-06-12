9982845b309e62bb526d6091656677c65dd6fe8f
linguochi <382150914@qq.com>
2018-06-12 14:30:26 +0800

    1.0.3

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,13 @@ module.exports = {
     },
   },
   externals: {
-    vue: 'vue',
+    vue: {
+      root: 'Vue',
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue',
+    },
+    axios: 'axios',
   },
   module: {
     rules: [
